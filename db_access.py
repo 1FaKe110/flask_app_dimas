37b5866fb7a5ccd84ab1ac1b03e8aade70f06917
1FaKe110 <gabko2016@gmail.com>
2022-05-13 11:18:52 +0300

    ///

--- db_access.py
+++ db_access.py
@@ -3,17 +3,28 @@ import sqlite3
 con = sqlite3.connect("Users.sqlite")
 cursor = con.cursor()
 
-query = """ Create table Users (
+# query = """ Create table Users (
+#     id integer PRIMARY KEY AUTOINCREMENT,
+#     username text NOT NULL,
+#     password text NOT NULL,
+#     email text NOT NULL,
+#     address text NOT NULL,
+#     phone text NOT NULL
+# )
+# """
+
+query = """ Create table Houses (
     id integer PRIMARY KEY AUTOINCREMENT,
     username text NOT NULL,
-    password text NOT NULL,
-    email text NOT NULL,
-    address text NOT NULL,
-    phone text NOT NULL
+    house_name text NOT NULL,
+    structure_type text NOT NULL,
+    sensor_type text NOT NULL,
+    sensor_ui_value text
 )
 """
-cursor.execute(query)
 
+# query = "DROP TABLE Houses;"
+cursor.execute(query)
 
 
 def main():
